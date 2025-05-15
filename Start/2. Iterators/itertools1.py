d75754d276f9fd89189ac5e4f947d299c586b6b4
rnogueira73 <ruben_nogueira@hotmail.com>
2025-05-15 16:30:55 -0600

    Update to iterators

--- Start/2. Iterators/itertools1.py
+++ Start/2. Iterators/itertools1.py
@@ -20,8 +20,8 @@ counter = itertools.count(100,10)
 # print(next(counter))
 # accumulate creates an iterator that accumulates values
 vals = [10,20,30,40,50,40,30]
-# acc = itertools.accumulate(vals, max)
-# print(list(acc))
+acc = itertools.accumulate(vals, max)
+print(list(acc))
 
 # amortize a loan over a set number of payments for a 2000 loan at 4%
 payments = [100, 125, 200, 105, 100, 120, 110, 130, 150, 100, 110, 120]
