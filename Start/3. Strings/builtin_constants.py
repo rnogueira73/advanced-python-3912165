b9001053d0ae164e556c8b029416092828022cbf
rnogueira73 <ruben_nogueira@hotmail.com>
2025-05-20 11:52:25 -0600

    String Procesing

--- Start/3. Strings/builtin_constants.py
+++ Start/3. Strings/builtin_constants.py
@@ -2,30 +2,37 @@
 # Using the built-in string constants
 
 import string
-
+import secrets
 
 # built-in constants for a variety of needs
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.digits)
-print(string.hexdigits)
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
+# print(string.digits)
+# print(string.hexdigits)
 print(string.punctuation)
 
 
 # Define a test string
-testStr = "The quick brown fox jumps OVER the lazy dog."
+testStr = "The quick brown fox jumps OVER the lazy dog"
 
 # use an iterator to see if a string contains any punctuation 
-
-
+# if any(c in string.punctuation for c in testStr):
+#   print('The string contains punctiation')
+# else:
+#   print('No punctuation found')
 # generate a secure random password
-
+alphabet = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(alphabet) for _ in range(10))
+# print(password)
 
 # Check the strength of a password
 def check_password_strength(testPass):
-  pass
+  if (len(testPass) >=10 and any(char in string.punctuation for char in testPass) and any(char in string.digits for char in testPass) and any(char in string.ascii_letters for char in testPass)):
+    return f'{testPass} is a strong password'
+  else:
+    return f'{testPass} is not a strong password'
 
-# print(check_password_strength("MyTestPa$$123!"))
-# print(check_password_strength("password"))
-# print(check_password_strength("pa$$w0rd!"))
+print(check_password_strength("MyTestPa$$123!"))
+print(check_password_strength("password"))
+print(check_password_strength("pa$$w0rd!"))
